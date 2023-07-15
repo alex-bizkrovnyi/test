71ee206b2cd937ed958a89d705da038318880d8d
Anastasia <anastasia@Bizkrovnyi-MacBook-Pro.local>
2023-07-15 11:58:22 +0300

    Initial commit

---
+++ java1/DZ.java
@@ -0,0 +1,8 @@
+public class DZ {
+    public static void main(String[] args) {
+        String name = "Anastasia";
+        float weight = 50;
+        int age = 24;
+        System.out.println("Name: " + name + ". Weight: " + weight + ". Age: " + age + ". ");
+    }
+}
